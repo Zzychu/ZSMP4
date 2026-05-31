3ec42626d48c1cf48f06119cec4e5bed866e434b
Zzychu <kacper.zych2006@gmail.com>
2026-05-31 20:43:30 +0200

    Voxy files now come from outside server in a zip

--- Installator.bat
+++ Installator.bat
@@ -26,6 +26,9 @@ cd git
 curl -L https://github.com/git-for-windows/git/releases/download/v2.54.0.windows.1/MinGit-2.54.0-64-bit.zip --output git.zip
 tar -xf git.zip
 cd ..
+"git/cmd/git.exe" init
+"git/cmd/git.exe" config --global core.safecrlf false
+"git/cmd/git.exe" config --local core.safecrlf false
 "git/cmd/git.exe" clone https://github.com/Zzychu/ZSMP4.git
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\ZSMP4 ./
 
@@ -42,7 +45,7 @@ cd %APPDATA%
 cd .minecraft
 ren launcher_profiles.json launcher_profiles2.json
 cd %APPDATA%
-xcopy /S /Y .minecraft .minecraft_backup
+xcopy /S /Y /F .minecraft .minecraft_backup
 rmdir /S /Q .minecraft
 ren .minecraft_backup .minecraft
 cd %OriginalDirectory%/InstallFiles
@@ -56,13 +59,14 @@ cd %APPDATA%/.minecraft
 mkdir ZSMP4
 cd ZSMP4
 mkdir mods
-mkdir .voxy
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\mods mods\
-xcopy /S /Y /F %OriginalDirectory%\InstallFiles\.voxy .voxy\
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\java java\
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\config config\
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\options.txt ./
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\servers.dat ./
+curl -L http://zychuhost.ddns.net/.voxy.zip --output voxy.zip
+tar -xf voxy.zip
+del voxy.zip
 goto end
 
 :Update
--- RemoveFromLauncherList.ps1
+++
@@ -1,4 +0,0 @@
-$path = "$env:APPDATA\.minecraft\launcher_profiles.json"
-$json = Get-Content $path | ConvertFrom-Json
-$json.profiles.PSObject.Properties.Remove('ZSMP4')
-$json | ConvertTo-Json -Depth 100 | Set-Content $path
