87eab5a2a163a05c838d989f005c2e7d252070ba
Zzychu <kacper.zych2006@gmail.com>
2026-06-01 19:31:04 +0200

    fixed install and added update

--- Installator.bat
+++ Installator.bat
@@ -1,21 +1,34 @@
 @echo off
 set OriginalDirectory=%appdata%
 cd %OriginalDirectory%
-echo 1 - Install, 2 - Update, 3 - Uninstall
-set /p opcja=""
+mkdir tempZSMPInstaller
 
-
+:info
+cls
+echo ========================================================================================================================
+echo 1 - Install [ Instaluje paczke ZSMP4 na komputerze ] 
+echo 2 - Update [ Pobiera aktualizacje modow ]
+echo 3 - Uninstall [ Odinstalowywuje paczke i resetuje ustawienia ]
+echo 4 - EXIT
+echo ========================================================================================================================
+set /p opcja="Wybierz numer: "
 if /i "%opcja%"=="1" goto Install
 if /i "%opcja%"=="2" goto Update
 if /i "%opcja%"=="3" goto Uninstall
 
 :Install
 echo Wybrano install
+taskkill /F /IM "Minecraft*"
+@REM Odinstalowanie istniejącej paczki
+powershell -NoProfile -ExecutionPolicy remotesigned -Command "$path = \"$env:APPDATA\.minecraft\launcher_profiles.json\"; $json = Get-Content $path | ConvertFrom-Json; $json.profiles.PSObject.Properties.Remove('ZSMP4'); $json | ConvertTo-Json -Depth 100 | Set-Content $path"
+rmdir /S /Q %appdata%\.minecraft\ZSMP4
+
+rmdir /S /Q InstallFiles
 mkdir InstallFiles
 cd InstallFiles
 
 @REM Java
-curl -L https://download.oracle.com/java/21/archive/jdk-21.0.10_windows-x64_bin.zip --output zip.zip
+curl --ssl-no-revoke -L https://download.oracle.com/java/21/archive/jdk-21.0.10_windows-x64_bin.zip --output zip.zip
 tar -xf zip.zip
 ren jdk-21.0.10 java
 
@@ -23,7 +36,7 @@ ren jdk-21.0.10 java
 @REM Git stuff
 mkdir git
 cd git
-curl -L https://github.com/git-for-windows/git/releases/download/v2.54.0.windows.1/MinGit-2.54.0-64-bit.zip --output git.zip
+curl --ssl-no-revoke -L https://github.com/git-for-windows/git/releases/download/v2.54.0.windows.1/MinGit-2.54.0-64-bit.zip --output git.zip
 tar -xf git.zip
 cd ..
 "git/cmd/git.exe" init
@@ -55,36 +68,49 @@ xcopy /S /Y /F %OriginalDirectory%\InstallFiles\java java\
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\config config\
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\options.txt ./
 xcopy /S /Y /F %OriginalDirectory%\InstallFiles\servers.dat ./
-curl -L http://zychuhost.ddns.net/.voxy.zip --output voxy.zip
+curl --ssl-no-revoke -L http://zychuhost.ddns.net/.voxy.zip --output voxy.zip
 tar -xf voxy.zip
 del voxy.zip
 goto end
 
 :Update
 echo Wybrano update
+taskkill /F /IM "Minecraft*"
 mkdir UpdateFiles
 cd UpdateFiles
-
 
 @REM Git stuff
 mkdir git
 cd git
-curl -L https://github.com/git-for-windows/git/releases/download/v2.54.0.windows.1/MinGit-2.54.0-64-bit.zip --output git.zip
+curl --ssl-no-revoke -L https://github.com/git-for-windows/git/releases/download/v2.54.0.windows.1/MinGit-2.54.0-64-bit.zip --output git.zip
 tar -xf git.zip
 cd ..
 "git/cmd/git.exe" clone https://github.com/Zzychu/ZSMP4.git
 xcopy /S /Y /F %OriginalDirectory%\UpdateFiles\ZSMP4 ./
-
-
+cd %appdata%/.minecraft/ZSMP4
+rmdir /S /Q mods
+xcopy /S /Y /F %OriginalDirectory%\UpdateFiles\mods mods\
+robocopy %OriginalDirectory%\UpdateFiles\config config\ /E /XC /XO /XN
 goto end
+
+
 
 :Uninstall
+taskkill /F /IM "Minecraft*"
 powershell -NoProfile -ExecutionPolicy remotesigned -Command "$path = \"$env:APPDATA\.minecraft\launcher_profiles.json\"; $json = Get-Content $path | ConvertFrom-Json; $json.profiles.PSObject.Properties.Remove('ZSMP4'); $json | ConvertTo-Json -Depth 100 | Set-Content $path"
 rmdir /S /Q %appdata%\.minecraft\ZSMP4
-goto end
+rmdir /S /Q UpdateFiles
+rmdir /S /Q InstallFiles
+rmdir /S /Q tempZSMPInstaller
+goto info
+
+
 
 :end
 cd %OriginalDirectory%
 rmdir /S /Q UpdateFiles
 rmdir /S /Q InstallFiles
+rmdir /S /Q tempZSMPInstaller
+cls
 pause
+exit
